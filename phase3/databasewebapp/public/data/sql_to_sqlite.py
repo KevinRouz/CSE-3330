e0bf2cd27f13e28b08eaa271688083c756b1a87d
Kevin Farokhrouz <kevinafarokhrouz@gmail.com>
2024-11-14 16:58:07 -0600

    add sqlite db

---
+++ phase3/databasewebapp/public/data/sql_to_sqlite.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+# Open SQLite database connection
+conn = sqlite3.connect("project2.sqlite")
+cursor = conn.cursor()
+
+with open("project2.sql", "r") as sql_file:
+    sql_script = sql_file.read()
+
+# Execute the SQL script
+cursor.executescript(sql_script)
+
+conn.commit()
+conn.close()
